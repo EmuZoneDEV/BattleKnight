ab963e2db498438c955be13bd9f5cbd44447407f
EmuZoneDEV <52322805+EmuZoneDEV@users.noreply.github.com>
2019-07-03 00:00:42 +0200

    Update de.php

--- language/de.php
+++ language/de.php
@@ -1,1 +1,50 @@
-
+<?php
+$lang = array();
+//buildings info
+//Grain milll
+$lang['buildings'][0][0][0]="Ферма зерна";
+$lang['buildings'][0][0][1]="<center>Фермы выращивают урожай. С развитием фермы увеличивается ее производительность.</center>";
+//Lumber mill
+$lang['buildings'][0][1][0]="Лесопилка";
+$lang['buildings'][0][1][1]="<center>Лесопилка производит древесину.С развитием лесопилки увеличивается ее производительность.";
+//Smason
+$lang['buildings'][0][2][0]="Каменоломня";
+$lang['buildings'][0][2][1]="<center>В каменоломне производиться камень. С развитием каменоломни увеличивается ее производительность.";
+//Iron foundry
+$lang['buildings'][0][3][0]="Iron foundry-Ore site";
+$lang['buildings'][0][3][1]="<center>At the iron foundry ore sites are created and upgraded. Ore sites produce iron.";
+//Granary
+$lang['buildings'][0][4][0]="зернохранилище";
+$lang['buildings'][0][4][1]="<center>В зернохранилище хранится урожай.";
+//warehouse
+$lang['buildings'][0][5][0]="Склад";
+$lang['buildings'][0][5][1]="<center>На складе хранится сырье: древесина, камень и железо.";
+//Cache
+$lang['buildings'][0][6][0]="Тайник";
+$lang['buildings'][0][6][1]="<center>Тайник сохраняет определенное количество ресурсов, защищая их от вражеских набегов.";
+//Town Hall
+$lang['buildings'][0][7][0]="Ратуша";
+$lang['buildings'][0][7][1]="At the town hall you can build other structures and set taxes. The higher the level the faster constructions are completed and the bigger your vault will be.";
+//House
+$lang['buildings'][0][8][0]="Дом";
+$lang['buildings'][0][8][1]="<center>Здесь ты предоставляешь жилье для своего населения.";
+//Embassy
+$lang['buildings'][0][9][0]="Альянс";
+$lang['buildings'][0][9][1]="Посольство позволяет тебе создавать Альянс.";
+//Marketplace
+$lang['buildings'][0][10][0]="Рынок";
+$lang['buildings'][0][10][1]="Рынок позволяет тебе обменивать ресурсы и/или оружие с другими игроками. С развитием рынка увеличивается его функции.";
+//Cathedral
+$lang['buildings'][0][11][0]="Собор";
+$lang['buildings'][0][11][1]="<center>Собор повышает мораль городского народа.";
+//Port
+$lang['buildings'][0][12][0]="Замок";
+$lang['buildings'][0][12][1]="Замок - это место  где ты управляешь всей империей. Здесь ты можешь построить, разрушить или покинуть деревни.";
+$lang['buildings'][0][12][2]="Здесь ты можешь перенести свою столицу.";
+$lang['buildings'][0][12][3]="Здесь ты можешь построить новую деревню. Только напиши называние деревни и координаты.";
+//Wall
+$lang['buildings'][0][13][0]="Стена";
+$lang['buildings'][0][13][1]="Стена - защитная конструкция. Она повышает обороноспособность города в случае нападения.";
+//Tower
+$lang['buildings'][0][14][0]="Башня";
+$lang['buildings'][0][14][1]="Башня - атакующая конструкция. Она повышает атакующие способности города в случае нападения.";
