64411bccc55eaef48ea80d1c00c42a6b174b618d
EmuZoneDEV <52322805+EmuZoneDEV@users.noreply.github.com>
2019-07-03 00:10:11 +0200

    Update de.php

--- language/de.php
+++ language/de.php
@@ -2,49 +2,49 @@
 $lang = array();
 //buildings info
 //Grain milll
-$lang['buildings'][0][0][0]="Ферма зерна";
-$lang['buildings'][0][0][1]="<center>Фермы выращивают урожай. С развитием фермы увеличивается ее производительность.</center>";
+$lang['buildings'][0][0][0]="Getreidefarm";
+$lang['buildings'][0][0][1]="<center>Bauernhöfe bauen ihre Ernte an. Mit der Entwicklung der Farm steigt die Produktivität.</center>";
 //Lumber mill
-$lang['buildings'][0][1][0]="Лесопилка";
-$lang['buildings'][0][1][1]="<center>Лесопилка производит древесину.С развитием лесопилки увеличивается ее производительность.";
+$lang['buildings'][0][1][0]="Sägewerk";
+$lang['buildings'][0][1][1]="<center>Das Sägewerk produziert Holz und steigert mit der Entwicklung von Sägewerken die Produktivität.";
 //Smason
-$lang['buildings'][0][2][0]="Каменоломня";
+$lang['buildings'][0][2][0]="Steinbruch";
 $lang['buildings'][0][2][1]="<center>В каменоломне производиться камень. С развитием каменоломни увеличивается ее производительность.";
 //Iron foundry
 $lang['buildings'][0][3][0]="Iron foundry-Ore site";
 $lang['buildings'][0][3][1]="<center>At the iron foundry ore sites are created and upgraded. Ore sites produce iron.";
 //Granary
-$lang['buildings'][0][4][0]="зернохранилище";
+$lang['buildings'][0][4][0]="Getreidespeicher";
 $lang['buildings'][0][4][1]="<center>В зернохранилище хранится урожай.";
 //warehouse
-$lang['buildings'][0][5][0]="Склад";
+$lang['buildings'][0][5][0]="Lagerhaus";
 $lang['buildings'][0][5][1]="<center>На складе хранится сырье: древесина, камень и железо.";
 //Cache
-$lang['buildings'][0][6][0]="Тайник";
+$lang['buildings'][0][6][0]="Versteck";
 $lang['buildings'][0][6][1]="<center>Тайник сохраняет определенное количество ресурсов, защищая их от вражеских набегов.";
 //Town Hall
-$lang['buildings'][0][7][0]="Ратуша";
+$lang['buildings'][0][7][0]="Rathaus";
 $lang['buildings'][0][7][1]="At the town hall you can build other structures and set taxes. The higher the level the faster constructions are completed and the bigger your vault will be.";
 //House
-$lang['buildings'][0][8][0]="Дом";
+$lang['buildings'][0][8][0]="Haus";
 $lang['buildings'][0][8][1]="<center>Здесь ты предоставляешь жилье для своего населения.";
 //Embassy
-$lang['buildings'][0][9][0]="Альянс";
+$lang['buildings'][0][9][0]="Allianz";
 $lang['buildings'][0][9][1]="Посольство позволяет тебе создавать Альянс.";
 //Marketplace
-$lang['buildings'][0][10][0]="Рынок";
-$lang['buildings'][0][10][1]="Рынок позволяет тебе обменивать ресурсы и/или оружие с другими игроками. С развитием рынка увеличивается его функции.";
+$lang['buildings'][0][10][0]="Marktplatz";
+$lang['buildings'][0][10][1]="Auf dem Markt können Sie Ressourcen und / oder Waffen mit anderen Spielern austauschen. Mit der Entwicklung des Marktes nehmen seine Funktionen zu.";
 //Cathedral
-$lang['buildings'][0][11][0]="Собор";
-$lang['buildings'][0][11][1]="<center>Собор повышает мораль городского народа.";
+$lang['buildings'][0][11][0]="Kathedrale";
+$lang['buildings'][0][11][1]="<center>Der Glaube erhöht die Moral der Stadtbevölkerung.";
 //Port
-$lang['buildings'][0][12][0]="Замок";
-$lang['buildings'][0][12][1]="Замок - это место  где ты управляешь всей империей. Здесь ты можешь построить, разрушить или покинуть деревни.";
-$lang['buildings'][0][12][2]="Здесь ты можешь перенести свою столицу.";
-$lang['buildings'][0][12][3]="Здесь ты можешь построить новую деревню. Только напиши называние деревни и координаты.";
+$lang['buildings'][0][12][0]="Schloss";
+$lang['buildings'][0][12][1]="Das Schloss ist der Ort, an dem Sie das gesamte Reich kontrollieren. Hier kann man bauen, zerstören oder die Landschaft verlassen.";
+$lang['buildings'][0][12][2]="Hier können Sie Ihr Kapital verschieben.";
+$lang['buildings'][0][12][3]="Hier können Sie ein neues Dorf bauen. Schreiben Sie einfach den Namen des Dorfes und die Koordinaten.";
 //Wall
-$lang['buildings'][0][13][0]="Стена";
-$lang['buildings'][0][13][1]="Стена - защитная конструкция. Она повышает обороноспособность города в случае нападения.";
+$lang['buildings'][0][13][0]="Mauer";
+$lang['buildings'][0][13][1]="Die Mauer ist eine Schutzstruktur. Es erhöht die Verteidigung der Stadt im Falle eines Angriffs.";
 //Tower
-$lang['buildings'][0][14][0]="Башня";
-$lang['buildings'][0][14][1]="Башня - атакующая конструкция. Она повышает атакующие способности города в случае нападения.";
+$lang['buildings'][0][14][0]="Bergfried";
+$lang['buildings'][0][14][1]="Der Bergfried ist eine angreifende Struktur. Es erhöht die Angriffsfähigkeiten der Stadt im Falle eines Angriffs.";
